4b79b76dc3aae274e355361991971fa83a14290c
Harshal <90245661+harshalpatel42@users.noreply.github.com>
2024-09-26 11:11:01 +0530

    Add files via upload

---
+++ commentLines/comLines.c
@@ -0,0 +1,10 @@
+/* Hello this is a comment in a C program
+this is a sample program for counting the number of comment lines in lex*/
+
+#include<stdio.h>
+
+int main() {
+    printf("this line won't be counted");
+    //but this will be counted
+    return 0;
+}
